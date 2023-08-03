5e8c1996ae2770b15135230df4a6aac489b8cb55
tanughate05 <93458822+tanughate05@users.noreply.github.com>
2023-08-03 18:07:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // your code here
+document.getElementById('urlForm').addEventListener('submit', function (event) {
+            event.preventDefault(); // Prevent form submission and page refresh
+
+            const name = document.getElementById('name').value;
+            const year = document.getElementById('year').value;
+
+            // Update the h3 element with the new URL
+            document.getElementById('url').textContent = `https://localhost:8080/?name=${encodeURIComponent(name)}&year=${year}`;
+        });
